0cd2e9bca8f73d672c513f70dbb3b53075c35892
salikbubu <salikbula@gmail.com>
2023-05-15 10:34:05 +0200

    examples.js opgave 2

--- assets/js/opgaver/examples.js
+++ assets/js/opgaver/examples.js
@@ -1,30 +1,34 @@
 let myExtras = [];
 const myExtrasInput = document.getElementById('extraUdstyr');
 const myExtrasListElement = document.getElementById('extraList');
-
+const removeExtrasInput = document.getElementById('removeExtraUdstyr');
 
 myExtrasInput.addEventListener("change", addExtra);
+removeExtrasInput.addEventListener("change", removeExtra);
 
-
-
-function addExtra() {
-    /* din map kode her... */
+function addExtra(e) {
+    const selectedExtra = e.target.value;
+    myExtras.push(selectedExtra);
+    updateExtras();
+    updateRemoveExtras();
 }
 
-
+function removeExtra(e) {
+    const selectedExtra = e.target.value;
+    const index = myExtras.indexOf(selectedExtra);
+    if (index > -1) {
+        myExtras.splice(index, 1);
+    }
+    updateExtras();
+    updateRemoveExtras();
+}
 
 function updateExtras() {
-
-
-
-
-    /* din map kode her... */
-
-
+    const extrasHTML = myExtras.map(extra => `<li>${extra}</li>`).join('');
+    myExtrasListElement.innerHTML = extrasHTML;
 }
 
-
-
-
-
-
+function updateRemoveExtras() {
+    const removeExtrasHTML = myExtras.map(extra => `<option value="${extra}">${extra}</option>`).join('');
+    removeExtrasInput.innerHTML = `<option value="" disabled selected>vælg fra listen</option>` + removeExtrasHTML;
+}
