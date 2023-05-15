108ca786456e2caaca02e212e2260ec577741b66
salikbubu <salikbula@gmail.com>
2023-05-15 08:52:24 +0200

    first

--- assets/js/opgaver/arrays.js
+++ assets/js/opgaver/arrays.js
@@ -1,12 +1,32 @@
 /* arrays */
 
 //Opgave 1
-const myArray =/*din array her*/;
+const myArray = [
+    {
+        label: "element1",
+    },
+    {
+        label: "element2",
+    },
+    {
+        label: "element3",
+    },
+    {
+        label: "element4",
+    },
+    {
+        label: "element5",
+    },
+]
 
 
+// opgave 1.2 console.logs
+// console.log(myArray)
 
-
-// opgave 2 console.logs
-
-
+//Opgave 2
+console.log(myArray[0].label); // element1
+console.log(myArray[1].label); // element2
+console.log(myArray[2].label); // element3
+console.log(myArray[3].label); // element4
+console.log(myArray[4].label); // element5
 
